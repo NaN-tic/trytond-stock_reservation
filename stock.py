3e820a4974f3d33df0f459a36d09df3f4f41d3fc
??ngel ??lvarez <angel@nan-tic.com>
2016-06-17 15:38:42 +0200

    delete commented code

--- stock.py
+++ stock.py
@@ -20,9 +20,8 @@ from trytond.rpc import RPC
 __all__ = ['Reservation', 'WaitReservation', 'WaitReservationStart',
     'CreateReservations', 'CreateReservationsStart',
     'PrintReservationGraphStart', 'PrintReservationGraph', 'ReservationGraph',
-    'Move', 'PurchaseLine', 'Production', 'Sale',
-    'ShipmentOut', 'ShipmentOutReturn', 'ShipmentIn', 'ShipmentInternal',
-    'Purchase', 'PurchaseRequest']
+    'Move', 'Production', 'Sale',
+    'ShipmentOut', 'ShipmentOutReturn', 'ShipmentIn', 'ShipmentInternal',]
 __metaclass__ = PoolMeta
 
 STATES = {
@@ -1631,35 +1630,35 @@ class Move:
         super(Move, cls).delete(moves)
 
 
-class PurchaseLine:
-    __name__ = 'purchase.line'
+# class PurchaseLine:
+#     __name__ = 'purchase.line'
 
-    purchase_date = fields.Function(fields.Date('Purchase Date'),
-        'get_purchase_date')
+#     purchase_date = fields.Function(fields.Date('Purchase Date'),
+#         'get_purchase_date')
 
-    def get_purchase_date(self, name):
-        if self.purchase:
-            return self.purchase.purchase_date
+#     def get_purchase_date(self, name):
+#         if self.purchase:
+#             return self.purchase.purchase_date
 
-    @staticmethod
-    def order_purchase_date(tables):
-        pool = Pool()
-        Purchase = pool.get('purchase.purchase')
-        line, _ = tables[None]
-        if 'purchase' not in tables:
-            purchase = Purchase.__table__()
-            tables['purchase'] = {
-                None: (purchase, line.purchase == purchase.id),
-                }
-        else:
-            purchase = tables['purchase']
-        return Purchase.purchase_date.convert_order('purchase_date',
-            tables['purchase'], Purchase)
+#     @staticmethod
+#     def order_purchase_date(tables):
+#         pool = Pool()
+#         Purchase = pool.get('purchase.purchase')
+#         line, _ = tables[None]
+#         if 'purchase' not in tables:
+#             purchase = Purchase.__table__()
+#             tables['purchase'] = {
+#                 None: (purchase, line.purchase == purchase.id),
+#                 }
+#         else:
+#             purchase = tables['purchase']
+#         return Purchase.purchase_date.convert_order('purchase_date',
+#             tables['purchase'], Purchase)
 
-    @classmethod
-    def delete(cls, lines):
-        delete_related_reservations(lines, 'source_document')
-        super(PurchaseLine, cls).delete(lines)
+#     @classmethod
+#     def delete(cls, lines):
+#         delete_related_reservations(lines, 'source_document')
+#         super(PurchaseLine, cls).delete(lines)
 
 
 class ReserveRelatedMixin:
@@ -2126,150 +2125,3 @@ class ShipmentInternal:
     def delete(cls, shipments):
         delete_related_reservations(shipments, 'source_document')
         super(ShipmentInternal, cls).delete(shipments)
-
-
-class Purchase:
-    __name__ = 'purchase.purchase'
-    sales = fields.Function(fields.One2Many('sale.sale', None, 'Sales'),
-        'get_sales', searcher='search_sales')
-
-    def get_sales(self, name):
-        pool = Pool()
-        Reservation = pool.get('stock.reservation')
-        SaleLine = pool.get('sale.line')
-        ShipmentOut = pool.get('stock.shipment.out')
-        sales = set()
-        reservations = []
-
-        def get_recursive_moves(moves, processed_moves=None):
-            if not moves:
-                return []
-            if processed_moves is None:
-                processed_moves = set()
-
-            reservations = Reservation.search([
-                    ('source', 'in', moves),
-                    ])
-            new_moves = set([x.destination for x in
-                    reservations if x.destination])
-            for move in moves:
-                if move.id in processed_moves:
-                    continue
-                if move.production_input:
-                    new_moves |= set(move.production_input.outputs)
-                processed_moves.add(move.id)
-
-            reservations += get_recursive_moves(list(new_moves),
-                processed_moves)
-
-            return reservations
-
-        moves = []
-        for shipment in self.shipments + self.shipment_returns:
-            moves += shipment.inventory_moves
-        reservations = get_recursive_moves(moves)
-
-        direct_reservations = Reservation.search([
-                ('source_document', 'in', [str(l) for l in self.lines]),
-                ])
-
-        reservations += direct_reservations
-        reservations += get_recursive_moves([x.destination for x in
-                direct_reservations if x.destination])
-
-        for reservation in reservations:
-            if reservation.destination:
-                if reservation.destination.origin:
-                    origin = reservation.destination.origin
-                    if isinstance(origin, SaleLine):
-                        sales.add(origin.sale)
-                elif reservation.destination_document:
-                    dest_move = reservation.destination
-                    shipment = reservation.destination_document
-                    if isinstance(shipment, ShipmentOut):
-                        for move in shipment.outgoing_moves:
-                            if not move.product == dest_move.product:
-                                continue
-                            if move.origin and isinstance(move.origin,
-                                    SaleLine):
-                                sales.add(move.origin.sale)
-
-        return [s.id for s in sales]
-
-    @classmethod
-    def search_sales(cls, name, clause):
-        pool = Pool()
-        Sale = pool.get('sale.sale')
-        purchases = set()
-        for sale in Sale.search([('id',) + tuple(clause[1:])]):
-            for purchase in sale.purchases:
-                purchases.add(purchase.id)
-        return [('id', 'in', purchases)]
-
-
-class PurchaseRequest:
-    __name__ = 'purchase.request'
-    sales = fields.Function(fields.One2Many('sale.sale', None, 'Sales'),
-        'get_sales', searcher='search_sales')
-
-    def get_sales(self, name):
-        pool = Pool()
-        Reservation = pool.get('stock.reservation')
-        SaleLine = pool.get('sale.line')
-        ShipmentOut = pool.get('stock.shipment.out')
-        sales = set()
-        reservations = Reservation.search([
-                ('source_document', '=', str(self)),
-                ])
-
-        for reservation in reservations:
-            if reservation.destination:
-                if reservation.destination.origin:
-                    origin = reservation.destination.origin
-                    if isinstance(origin, SaleLine):
-                        sales.add(origin.sale)
-                elif reservation.destination_document:
-                    dest_move = reservation.destination
-                    shipment = reservation.destination_document
-                    if isinstance(shipment, ShipmentOut):
-                        for move in shipment.outgoing_moves:
-                            if not move.product == dest_move.product:
-                                continue
-                            if move.origin and isinstance(move.origin,
-                                    SaleLine):
-                                sales.add(move.origin.sale)
-
-        return [s.id for s in sales]
-
-    @classmethod
-    def search_sales(cls, name, clause):
-        pool = Pool()
-        Sale = pool.get('sale.sale')
-        requests = set()
-        for sale in Sale.search([('id',) + tuple(clause[1:])]):
-            for request in sale.purchase_requests:
-                requests.add(request.id)
-        return [('id', 'in', requests)]
-
-    @classmethod
-    def write(cls, *args):
-        pool = Pool()
-        Reservation = pool.get('stock.reservation')
-        actions = iter(args)
-        super(PurchaseRequest, cls).write(*args)
-        for requests, value in zip(actions, actions):
-            if 'purchase_line' in value:
-                reserves = Reservation.search([
-                        ('state', 'in', ['draft', 'waiting']),
-                        ('source_document', 'in', [str(r) for r in requests]),
-                        ])
-                if reserves:
-                    Reservation.write(reserves, {
-                            'source_document': ('purchase.line,%d' %
-                                value.get('purchase_line')),
-                            })
-
-    @classmethod
-    def delete(cls, requests):
-        delete_related_reservations(requests, 'source_document')
-        super(PurchaseRequest, cls).delete(requests)
